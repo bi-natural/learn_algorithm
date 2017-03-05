f34e955cad044d499e385ba8d0596aa1352c3bba
Yang <Family@iMac.local>
2017-03-05 11:14:30 +0900

    assignment#2

---
+++ algs4/dequeue/RandomizedQueue.java
@@ -0,0 +1,15 @@
+/**
+ * assignment #2
+ *
+ */
+
+ public class RandomizedQueue<Item> implements Iterable<Item> {
+    public RandomizedQueue()                 // construct an empty randomized queue
+    public boolean isEmpty()                 // is the queue empty?
+    public int size()                        // return the number of items on the queue
+    public void enqueue(Item item)           // add the item
+    public Item dequeue()                    // remove and return a random item
+    public Item sample()                     // return (but do not remove) a random item
+    public Iterator<Item> iterator()         // return an independent iterator over items in random order
+    public static void main(String[] args)   // unit testing (optional)
+ }
